01b9e09aa2cf8dba8b6c2d7c5c27b603f035023c
Krit Dass <dasskrit@gmail.com>
2023-07-25 22:08:38 +0530

    Updated PyPi with macOS support

--- src/pytch/__main__.py
+++ src/pytch/__main__.py
@@ -30,7 +30,7 @@ def main():
         help="use an alternate distribution's logo",
     )
 
-    parser.add_argument("-v", "--version", action="version", version="1.1.0")
+    parser.add_argument("-v", "--version", action="version", version="1.1.1")
 
     args = parser.parse_args()
 
